99fd83e8cad09807b8257301097bf465c7755a02
David Charles <kingdavidchris01@gmail.com>
2026-08-21 11:48:24 +0100

    Trust all Vercel proxies to resolve Mixed Content/HTTP issues

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Trust all Vercel proxies to fix Mixed Content/HTTP issues
+        $middleware->trustProxies(at: '*');
+
         $middleware->alias([
             'can-manage-prices' => \App\Http\Middleware\EnsureCanManagePrices::class,
             'is-admin'          => \App\Http\Middleware\EnsureIsAdmin::class,
